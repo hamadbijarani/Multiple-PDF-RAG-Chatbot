b551fd00b79c8c257b9effa1469e98fafed096fb
Hamadullah . <hamadullahkhanbijarani@gmail.com>
2025-08-17 18:06:05 +0500

    Update deployed_app.py

--- deployed_app.py
+++ deployed_app.py
@@ -5,8 +5,6 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from pydantic import SecretStr
 from langchain.prompts import PromptTemplate
-from langchain_community.vectorstores import FAISS
-from langchain.callbacks.base import BaseCallbackHandler
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
